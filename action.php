9ab5fae626d85c4592ea25b31e6b924ce72bcede
Rohitphebbale <115656852+Rohitphebbale@users.noreply.github.com>
2023-01-18 17:16:06 +0530

    Add files via upload

---
+++ action.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <title>Include HTML</title>
+</head>
+<body>
+
+    <?php Include "header.HTML" ?>
+<p>Hello world</p>
+    <?php Include "footer.HTML" ?>
+
+</body>
+</html>
