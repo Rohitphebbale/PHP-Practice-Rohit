8a71df9ef90f7707fe922f39281e610fc18ac6f2
Rohitphebbale <115656852+Rohitphebbale@users.noreply.github.com>
2023-01-23 17:06:48 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -5,28 +5,26 @@
     <title>Document</title>
 </head>
 <body>
-    
-<h1>Simple Login form</h1>
-<form action="index.php" method="post">
-UserName : <input type="text" placeholder="Enter the Name" name="username"> <br> <br>
-Password : <input type="password" placeholder="Enter the Password" name="password"> <br> <br>
-<input type="submit" name="submit">
-</form>
+    <div class="container">
+        <h1>Registration form</h1>
+        <form action="index.php" method="post">
+            First Name : <input type="text" placeholder="Your Name" name="firstname"> <br><br>
+             Last Name : <input type="text" placeholder="Your lastName" name="lastname"> <br> <br>
+              Email : <input type="text" placeholder="Your Email" name="email"><br> <br>
+               Contact No : <input type="number" placeholder="Your Contact Number" name="contact"> <br><br>
+               Password : <input type="password" placeholder="Password" name="password"><br><br>
 
-<?php
-if(isset ($_POST["submit"])){
-    $un = $_POST["username"];
-    $pass = $_POST["password"];
+            <input type="submit" name="submit">
+        </form>
+    </div>
 
-    if($un == "admin" && $pass == "admin"){
-       header('Location: sam.php');
-       exit();
-        
-    }
-    else{
-        echo "invalid username & password";
-    }
-}
-?>
+    <div>
+        <?php
+
+        if(isset($_POST["submit"])){
+            echo "User Submit..";
+        }
+        ?>
+    </div>
 </body>
 </html>
