6829c6c13585e653916a3f5bc9b7fde2069fcf07
Rohitphebbale <115656852+Rohitphebbale@users.noreply.github.com>
2023-01-23 17:07:43 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,30 +1,111 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-   
+  
     <title>Document</title>
 </head>
 <body>
-    <div class="container">
-        <h1>Registration form</h1>
-        <form action="index.php" method="post">
-            First Name : <input type="text" placeholder="Your Name" name="firstname"> <br><br>
-             Last Name : <input type="text" placeholder="Your lastName" name="lastname"> <br> <br>
-              Email : <input type="text" placeholder="Your Email" name="email"><br> <br>
-               Contact No : <input type="number" placeholder="Your Contact Number" name="contact"> <br><br>
-               Password : <input type="password" placeholder="Password" name="password"><br><br>
 
-            <input type="submit" name="submit">
-        </form>
-    </div>
+<!-- =============================== PHP ========================== -->
 
-    <div>
-        <?php
+<?php
+// $nameErr = $emailErr = $genderErr = $websiteErr = "";
+// $name = $email = $gender = $comment = $website = "";
 
-        if(isset($_POST["submit"])){
-            echo "User Submit..";
-        }
-        ?>
-    </div>
+$nameErr = "";
+$emailErr = "";
+$genderErr ="";
+$websiteErr = "";
+$name = "";
+$email = "";
+$gender = "";
+$comment = "";
+$website = "";
+
+// if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+if(isset($_POST["submit"])){
+   
+    if(empty($_POST["name"])){
+        $nameErr = "Name is required";
+    }
+    else{
+        $name = test_input($_POST["name"]);
+    }
+    if(!preg_match("/^[a-z A-Z-' ]*$/",$name)){
+            $nameErr = "Only letters and white space allowed";
+    }
+}
+            // ------------------------------------
+    if(empty($_POST["email"])){
+        $emailErr = "Email is required";
+    }
+    else{
+        $email = test_input($_POST["email"]);
+    }
+     if( !filter_var($email, FILTER_VALIDATE_EMAIL)){
+        $emailErr = "Invalid email Format";
+    }
+        //   --------------------------------------      
+    if(empty($_POST["website"])){
+         $website = "";
+        // $websiteErr = "Invali URL";
+    }
+    else{
+        $website = test_input($_POST["website"]);
+    }
+    // if(!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)){
+    //     $websiteErr = "Invali URL";
+    // }
+        // ----------------------------------------------------
+
+    if(empty($_POST["comment"])){
+        $comment = "";
+    }
+    else{
+        $comment = text_input($_POST["comment"]);
+    }
+
+    // ------------------------------------------------------------
+
+    if(empty($_POST["gender"])){
+        $genderErr = "Gender is required";
+    }
+    else{
+        $gender = text_input($_POST["gender"]);
+    }
+?>
+
+
+    <!-- // ------------------------------------------------------------- -->
+    <!-- ============================ HTML =================== -->
+    <form action="index.php" method="post"> 
+    Name : <input type="text" name="name" value="<?php echo $name; ?>">
+    <small class="error">* <?php echo $nameErr; ?></small>
+    <br><br>
+    
+    E-mail : <input type="text" name="email" value="<?php echo $email; ?>">
+    <span class="error">* <?php echo $emailErr; ?></span>
+    <br><br>
+    
+    Website : <input type="text" name="website" value="<?php echo $website; ?>">
+    <span class="error">* <?php echo $websiteErr; ?></span>
+    <br><br>
+    
+    Comment : <textarea name="comment" id="comment" cols="30" rows="3">
+        <?php echo $comment; ?> </textarea>
+        <br><br>
+    
+    Gender : 
+        <input type="radio" name="gender" <?php if(isset($gender) && $gender == "Female") echo "checked"; ?>>Female;
+        <input type="radio" name="gender" <?php if(isset($gender) && $gender == "male") echo  "checked";?> >Male:
+        <input type="radio" name="gender" <?php if(isset($gender) && $gender == "Other") echo "checked"; ?>>Others;
+                <span class="error">* <?php echo $genderErr; ?></span> <br><br>
+    
+    <input type="submit" name="submit">
+    </form>
+    
+     
+
 </body>
 </html>
