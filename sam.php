8fa9ede77406837f094500a126fa2636e77bfa82
Rohitphebbale <115656852+Rohitphebbale@users.noreply.github.com>
2023-01-23 17:05:55 +0530

    Add files via upload

---
+++ sam.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   
+    <title>Document</title>
+</head>
+<body>
+<form action="sam.php" method="post">
+    <h1>well come to home page</h1>
+</form>
+</body>
+</html>
